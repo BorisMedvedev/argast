b4c8539330fcaf8384b6990d3728452c730d591e
Your Name <you@example.com>
2022-06-17 16:01:50 +0300

    777

--- js/js.js
+++ js/js.js
@@ -90,16 +90,16 @@ defaultSelect2();
 
 
 
-function show_hide_password(target){
-	var input = document.getElementById('password-input');
-	if (input.getAttribute('type') == 'password') {
-		target.classList.add('view');
-		input.setAttribute('type', 'text');
-	} else {
-		target.classList.remove('view');
-		input.setAttribute('type', 'password');
-	}
-	return false;
-}
+// function show_hide_password(target){
+// 	var input = document.getElementById('password-input');
+// 	if (input.getAttribute('type') == 'password') {
+// 		target.classList.add('view');
+// 		input.setAttribute('type', 'text');
+// 	} else {
+// 		target.classList.remove('view');
+// 		input.setAttribute('type', 'password');
+// 	}
+// 	return false;
+// }
 
 
